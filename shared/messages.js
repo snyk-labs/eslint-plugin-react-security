9fcbddc620ac21a6de0a418831b22bdd7cc636b4
Ron Perris <ron@perr.is>
2020-06-17 17:50:33 -0700

    feat: Added rule for findDOMNode() detection.

--- shared/messages.js
+++ shared/messages.js
@@ -1,4 +1,5 @@
 module.exports = {
   "no-dangerously-set-innerhtml": "dangrouslySetInnerHTML prop usage detected",
+  "no-find-dom-node": "refs prop usage detected",
   "no-refs": "refs prop usage detected",
 };
