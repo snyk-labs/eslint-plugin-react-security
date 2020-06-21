c79649fe7a3e484d33aff47a8ed5baf894238fc2
Ron Perris <ron@perr.is>
2020-06-21 11:34:04 -0700

    feat: Added rule for javascript: url detection. #3

--- shared/messages.js
+++ shared/messages.js
@@ -1,5 +1,6 @@
 module.exports = {
   "no-dangerously-set-innerhtml": "dangrouslySetInnerHTML prop usage detected",
+  "no-javascript-urls": "javascript: url in JSX detected",
   "no-find-dom-node": "findDOMNode usage detected",
   "no-refs": "refs prop usage detected",
 };
